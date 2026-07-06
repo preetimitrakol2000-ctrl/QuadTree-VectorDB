5928e5804e5b29122962aaf29d8023fe1dbdd490
Preeti Mitra <preeti.mitra.kol2000@gmail.com>
2026-07-07 00:10:40 +0530

    Integrate C library with ctypes and add methods
    Refactor quad_bridge.py to use ctypes for C library integration and implement spatial chunk loading and locating methods.

--- quad_bridge.py
+++ quad_bridge.py
@@ -1,11 +1,31 @@
-#ifndef QUADTREE_VECTOR_H
-#define QUADTREE_VECTOR_H
+import ctypes
+import os
+import sys
 
-typedef struct SpatialPoint SpatialPoint;
-typedef struct QuadNode QuadNode;
+class QuadBridge:
+    def __init__(self):
+        if not os.path.exists("./libquad.so") and not os.path.exists("./libquad.dll"):
+            if sys.platform.startswith("win"):
+                os.system("gcc -shared -o libquad.dll quadtree_vector.c")
+                lib_path = "./libquad.dll"
+            else:
+                os.system("gcc -shared -fPIC -o libquad.so quadtree_vector.c")
+                lib_path = "./libquad.so"
+        else:
+            lib_path = "./libquad.dll" if sys.platform.startswith("win") else "./libquad.so"
 
-QuadNode* init_quad_node(float mix, float miy, float max, float may);
-bool insert_spatial_point(QuadNode* node, float x, float y, int id);
-int query_spatial_point(QuadNode* node, float x, float y);
+        self.lib = ctypes.CDLL(lib_path)
+        self.lib.init_quad_node.restype = ctypes.c_void_p
+        self.lib.insert_spatial_point.argtypes = [ctypes.c_void_p, ctypes.c_float, ctypes.c_float, ctypes.c_int]
+        self.lib.insert_spatial_point.restype = ctypes.c_bool
+        self.lib.query_spatial_point.argtypes = [ctypes.c_void_p, ctypes.c_float, ctypes.c_float]
+        self.lib.query_spatial_point.restype = ctypes.c_int
+        
+        # Instantiate coordinate index bounds from 0.0 to 100.0
+        self.root_ptr = self.lib.init_quad_node(0.0, 0.0, 100.0, 100.0)
 
-#endif
+    def load_spatial_chunk(self, coord_x: float, coord_y: float, document_id: int):
+        self.lib.insert_spatial_point(self.root_ptr, coord_x, coord_y, document_id)
+
+    def locate_spatial_chunk(self, coord_x: float, coord_y: float) -> int:
+        return self.lib.query_spatial_point(self.root_ptr, coord_x, coord_y)
